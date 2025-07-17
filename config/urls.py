a10d3b3068351d867927023d3719e6d6381f6fa8
Jason Heppler <jason.heppler@gmail.com>
2025-07-17 10:20:39 -0500

    feat(account): Template files for account management, template files for 500, 403, 404 pages

--- config/urls.py
+++ config/urls.py
@@ -3,15 +3,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
+
 from mapping_violence.views import index
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('admin/', admin.site.urls),
-
+    path("", index, name="index"),
+    path("admin/", admin.site.urls),
     # allauth
-    path('accounts/', include('allauth.urls')),
-
+    path("accounts/", include("allauth.urls")),
+    path("accounts/profile/", TemplateView.as_view(template_name="profile.html")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
