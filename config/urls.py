5225d2f8a465bd1b2f9dae172c95063892a21ea8
Jason Heppler <jason.heppler@gmail.com>
2025-08-05 13:39:47 -0500

    feat: Add Wagtail and snippets to homepage content

--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 from mapping_violence.views import index
 
 urlpatterns = [
     path("", index, name="index"),
     path("admin/", admin.site.urls),
+    path("cms/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
     # allauth
     path("accounts/", include("allauth.urls")),
     path("accounts/profile/", TemplateView.as_view(template_name="profile.html")),
--- mapping_violence/views.py
+++ mapping_violence/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from content.models import HomePageContent, ProjectPerson
+
 
 def index(request):
-  return render(request, "index.html")
+    # Get active home page content
+    home_content = HomePageContent.objects.filter(is_active=True).first()
+
+    # Get project team and advisory board members
+    project_team = ProjectPerson.objects.filter(
+        is_active=True, team_type="project_team"
+    ).order_by("display_order", "name")
+
+    advisory_board = ProjectPerson.objects.filter(
+        is_active=True, team_type="advisory_board"
+    ).order_by("display_order", "name")
+
+    context = {
+        "home_content": home_content,
+        "project_team": project_team,
+        "advisory_board": advisory_board,
+    }
+
+    return render(request, "index.html", context)
