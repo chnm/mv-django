f658b1f02fc040e9680c3e7b6aedfe97e25c1561
qtrinh2 <qtrinh2@gmu.edu>
2026-02-20 13:36:35 -0500

    django-allauth slack patch

--- mapping_violence/apps.py
+++ mapping_violence/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mapping_violence"
+
+    def ready(self):
+        # Fix hardcoded double-slash bug in allauth's Slack provider
+        # https://slack.com//openid/connect/authorize → https://slack.com/openid/connect/authorize
+        from allauth.socialaccount.providers.slack.views import SlackOAuth2Adapter
+        SlackOAuth2Adapter.authorize_url = "https://slack.com/openid/connect/authorize"
+
